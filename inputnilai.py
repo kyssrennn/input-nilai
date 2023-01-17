fb923597fa1f2e163c49159b987247f87f7d62e6
kyssrennn <122344078+kyssrennn@users.noreply.github.com>
2023-01-17 13:41:20 +0700

    Add files via upload

---
+++ inputnilai.py
@@ -0,0 +1,12 @@
+nilai = int(input("Masukkan nilai siswa : "))
+
+if nilai >= 90 :
+    print("Nilai Tugas = A")
+elif nilai >= 80 :
+    print("Nilai Tugas = B")
+elif nilai >= 70 :
+    print("Nilai Tugas = C")
+elif nilai <= 60 :
+    print("Nilai Tugas = D")
+else:
+ print("Nilai Tugas = E")
